ae1fd57ea1b2c4c495bba37a265b8c7a110dc6a4
wipro <wipro@Manick-MAC.local>
2017-03-07 15:16:33 +0500

    Initial Commit

---
+++ WeatherForecastApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherForecastApp
+//
+//  Created by wipro on 07/03/17.
+//  Copyright (c) 2017 Wipro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherForecastApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherForecastApp
+//
+//  Created by wipro on 07/03/17.
+//  Copyright (c) 2017 Wipro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
